e54a8da8aa8e95225883dfb5199e096267b376e3
alkzlh <alekz.hummer.1996@gmail.com>
2018-10-23 08:41:40 -0500

    submaterias alex

---
+++ resources/views/sistema/altasubmaterias.blade.php
@@ -0,0 +1,27 @@
+@extends('templates.app')
+
+
+@section("body")	
+	<div class="row">
+		<div class="col-xs-12 col-md-12">
+
+			<div class="card">
+				<div class="card-header">
+					Submaterias
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">Submaterias</h5>
+					<form>
+						<input type="" name="" class="form-control" placeholder="Nombre">
+						<input type="" name="" class="form-control" placeholder="Clave">
+						<input type="" name="" class="form-control" placeholder="Juicio">
+						
+						<!-- <input type="submit" name="" class="btn btn-primary"> -->
+						<button class="btn btn-success">Guardar</button>
+						<button class="btn btn-warning">Cancelar</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop	
